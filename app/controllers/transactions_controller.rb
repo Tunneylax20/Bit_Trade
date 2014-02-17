4827347f6abcb5c5da297ca9e5e1440c61f281d4
tunneylax20 <bryantunney@gmail.com>
2014-02-17 15:59:43 -0500

    buy and sell now differentiated with positives and negative numbers in the transaction log

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,14 +1,19 @@
 class TransactionsController < ApplicationController
 
   before_action(:load_user, only: [:new, :create] )
+  before_action(:buy_and_sell_logic, only: [:new, :create])
 
   def new
 
   end
 
   def create
-    @transaction = Transaction.new({:price => params[:price], :btc_total => params[:btc_total], :usd_total => params[:usd_total], :buy => params[:buy], :user_id => params[:user_id]})
+    # btc_total = params[:btc_total].to_i - 1
+    # @transaction = Transaction.new({:price => params[:price], :btc_total => btc_total, :usd_total => params[:usd_total], :buy => params[:buy], :user_id => params[:user_id]})
+    # @transaction.save
+    @transaction = Transaction.new(:price => @price, :btc_total => @btc_total, :usd_total => @usd_total, :buy => @buy, :user_id => @user_id)
     @transaction.save
+
     redirect_to user_transaction_path(@user, @transaction)
   end
 
@@ -22,8 +27,18 @@ class TransactionsController < ApplicationController
     return @user = User.find(params[:user_id])
   end
 
-  def transaction_params
-    params.require(:transaction).permit(:price, :btc_total, :usd_total, :buy, :user_id)
-  end
+  def buy_and_sell_logic #(price, btc_total, usd_total, buy, user_id)
+    @price = params[:price].to_i
+    @buy = params[:buy].to_i
+    @user_id = params[:user_id]
 
+      if @buy == 0
+        @usd_total = params[:usd_total].to_i * (-1)
+        @btc_total = params[:btc_total].to_i
+      else 
+        @usd_total = params[:usd_total].to_i
+        @btc_total = params[:btc_total].to_i * (-1)
+      end
+
+  end
 end
