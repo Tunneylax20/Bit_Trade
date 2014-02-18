bf498bfc566686ce343431c65eb7e1801afba963
tunneylax20 <bryantunney@gmail.com>
2014-02-18 11:51:05 -0500

    user can see current balance

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -19,7 +19,7 @@ class TransactionsController < ApplicationController
 
   def show
     @transaction = @user.transactions
-    
+
   end
 
 
@@ -29,18 +29,28 @@ class TransactionsController < ApplicationController
     return @user = User.find(params[:user_id])
   end
 
-  def buy_and_sell_logic #(price, btc_total, usd_total, buy, user_id)
+  def buy_and_sell_logic
     @price = params[:price].to_i
     @buy = params[:buy].to_i
     @user_id = params[:user_id]
 
-      if @buy == 0
-        @usd_total = params[:usd_total].to_i * (-1)
-        @btc_total = params[:btc_total].to_i
-      else 
-        @usd_total = params[:usd_total].to_i
-        @btc_total = params[:btc_total].to_i * (-1)
-      end
+    if @buy == 0
+      @usd_total = params[:usd_total].to_i * (-1)
+      @btc_total = params[:btc_total].to_i
+    else 
+      @usd_total = params[:usd_total].to_i
+      @btc_total = params[:btc_total].to_i * (-1)
+    end
+
+    if -@usd_total > @user.current_usd_balance
+      flash[:error] = "Not enough USD in balance to complete this transaction!"
+      redirect_to user_path(@user)
+    end
+
+    if -@btc_total > @user.current_btc_balance
+      flash[:error] = "Can't sell Bitcoins you don't have!"
+      redirect_to user_path(@user)
+    end
 
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,23 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  def current_usd_balance
+    self.usd + Transaction.where(user: self).sum('usd_total').to_f
+    
+  end
 
+  def current_btc_balance
+    self.btc + Transaction.where(user: self).sum('btc_total').to_f
+    
+  end
+
+  def get_current_price
+    from_bitstamp = HTTParty.get("https://www.bitstamp.net/api/ticker/")
+    return from_bitstamp["last"].to_f
+  end
+
+  def total_balance
+    current_usd_balance + (current_btc_balance * get_current_price)
+  end
 
 end
