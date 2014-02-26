62d94c03c78dbac3e56ad353cdacf01e5614508e
tunneylax20 <bryantunney@gmail.com>
2014-02-26 06:39:52 -0500

    change Doge price from bitcoin to dollars

--- app/helpers/price_helper.rb
+++ app/helpers/price_helper.rb
@@ -1,7 +1,7 @@
 module PriceHelper
 
-  def doge_price_btc
-    return HTTParty.get("https://www.dogeapi.com/wow/?a=get_current_price&convert_to=BTC&amount_doge=1000")
+  def doge_price_usd
+    return HTTParty.get("https://www.dogeapi.com/wow/?a=get_current_price&convert_to=USD&amount_doge=1000")
   end
 
 
--- spec/features/doge_spec.rb
+++ spec/features/doge_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe "a user can buy doge coin" do
-  let!(:doge_purchase) { Transaction.create(price: 600, btc_total: 2, usd_total: 1200, buy: 0)}
+  let!(:user) { FactoryGirl.create(:user) }
+  let!(:doge_purchase) { Transaction.create(price: 0.000113, btc_total: 0, usd_total: (-1.13), buy: 0, doge: 10000) }
+
+
+
 end
