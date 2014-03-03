a9661be34839781c9ea8e608f89f9afbf84c29d1
tunneylax20 <bryantunney@gmail.com>
2014-03-03 09:28:39 -0500

    moved btc price to price helper. Deleted buy_sell_logic method that wasn't being used in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,46 +11,6 @@ class ApplicationController < ActionController::Base
     current_user.present?
   end
 
-  def get_current_price
-    from_bitstamp = HTTParty.get("https://www.bitstamp.net/api/ticker/")
-    return from_bitstamp["last"]
-  end
-
-  def buy_sell_logic
-      @price = params[:price].to_i
-      @buy = params[:buy].to_i
-      @user_id = params[:user_id]
-
-      if @buy == 0
-        @usd_total = params[:usd_total].to_i * (-1)
-        @btc_total = params[:btc_total].to_i
-      else 
-        @usd_total = params[:usd_total].to_i
-        @btc_total = params[:btc_total].to_i * (-1)
-      end
-
-      if -@usd_total > @user.current_usd_balance
-        flash[:error] = "Not enough USD in balance to complete this transaction!"
-        redirect_to user_path(@user)
-      end
-
-      if -@btc_total > @user.current_btc_balance
-        flash[:error] = "Can't sell Bitcoins you don't have!"
-        redirect_to user_path(@user)
-      end
-
-      if -@usd_total <= 0 && @btc_total > 0
-        flash[:error] = "Invalid Entry, but nice try"
-        redirect_to user_path(@user)
-      end
-
-      if @usd_total <= 0 && @btc_total < 0
-        flash[:error] = "Invalid Entry, but nice try"
-        redirect_to user_path(@user)
-      end
-
-    end
-
-  helper_method :current_user, :logged_in?, :get_current_price
+  helper_method :current_user, :logged_in?
 
 end
--- app/helpers/price_helper.rb
+++ app/helpers/price_helper.rb
@@ -4,6 +4,10 @@ module PriceHelper
     return HTTParty.get("https://www.dogeapi.com/wow/?a=get_current_price&convert_to=USD&amount_doge=1000")
   end
 
+  def get_current_price
+    from_bitstamp = HTTParty.get("https://www.bitstamp.net/api/ticker/")
+    return from_bitstamp["last"]
+  end
 
 
 
