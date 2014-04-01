951d7ff5fb23474356196f6e62d3f46e6babdf46
tunneylax20 <bryantunney@gmail.com>
2014-04-01 14:53:13 -0400

    user is logged in after signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,6 @@ before_action :authenticate, :authorize, only: [:edit, :update, :show]
   def create
     @user = User.new(user_params)
 
-
     if @user.save
       session[:user_id] = @user.id
       redirect_to user_path(@user)
